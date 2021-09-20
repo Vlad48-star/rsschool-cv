e961b44b65425a409b0527529ee3173b2b00f001
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2021-09-20 15:31:27 +0300

    init:Start my project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# rsschool-cv
+https://Vlad48-star.github.io/rsschool-cv/cv
---
+++ menu.js
@@ -0,0 +1,7 @@
+const svg = document.querySelector('.svg-menu');
+const menu = document.querySelector('.menu');
+
+
+svg.addEventListener('click', () =>{
+    menu.classList.add('menu-enable');
+})
